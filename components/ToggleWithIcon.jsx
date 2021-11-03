b08dd8340940ab495a46c81c578f01bb437a6f41
GDeDev <giulianodamico@gmail.com>
2021-11-02 22:48:25 -0300

    Build test dark mode & banner

--- components/ToggleWithIcon.jsx
+++ components/ToggleWithIcon.jsx
@@ -75,9 +75,9 @@ export default function Example() {
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
               />
             </svg>
