48a7d33c29a6082350bcf7f6f721e861865373b2
GDeDev <giulianodamico@gmail.com>
2021-11-09 22:35:04 -0300

    Build vercel

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,8 @@
 import React from "react";
-import SimpleWithLogo from "../components/SimpleWithLogo";
+import WithPopularPages from "./../components/WithPopularPages";
 
 const Blog = () => {
-  return <SimpleWithLogo />;
+  return <WithPopularPages />;
 };
 
 export default Blog;
