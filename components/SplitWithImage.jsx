1402399ac11e8c8d980ddd26cd05be6beb8c4e6d
GDeDev <giulianodamico@gmail.com>
2021-11-11 22:28:07 -0300

    Fix image

--- components/SplitWithImage.jsx
+++ components/SplitWithImage.jsx
@@ -12,7 +12,7 @@ export default function SplitWithImage({ index }) {
         <img
           className="w-full h-full object-cover"
           src={image}
-          alt="http://www.lasnieves.edu.ar/uploaded_files/a98t8295.jpg"
+          alt="ImageTailwind"
         />
       </div>
       <div className="relative max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
